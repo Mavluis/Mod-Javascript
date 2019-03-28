5018ad458e51c8ec41f4ebe3c137de4d15850c97
Luis Fernández <mavluis@gmail.com>
2019-03-28 17:40:04 +0100

    Modulo bucles y for

---
+++ bucles_for.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// bucle "hazlo hasta que no se pueda hacer en cuenta atras" //
+
+// let count = 50;
+
+// while (count >= 0) {
+//   console.log(count);
+//   count--;
+// }
+
+// bucle "hazlo hasta que llegues al valor de la condicion - positivo" //
+
+// let count = 0;
+
+// while (count <= 50) {
+//   console.log(count);
+//   count++;
+// }
+
+// for //
+
+let names =["antonio", "manuel", "miguel", "luis"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+  
+}
