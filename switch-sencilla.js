f5308a4a134c0dbf54304aa5f9162e8dea69ff7f
Luis Fernández <mavluis@gmail.com>
2019-03-28 16:56:50 +0100

    modulo switch sencillo

---
+++ switch-sencilla.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// condicion sencilla con "switch" //
+
+let algo = "bad";
+
+switch (algo) {
+  case "good":
+    console.log("Perfect");
+    break;
+  default:
+    console.log("Nulo");
+    break;
+  case "bad":
+    console.log("Imperfecto");
+}
+
